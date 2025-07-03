1e7e42f01fef998297d98118cf49bbd9a08fd0fc
dopeman27 <krzysztof.maksim1@gmail.com>
2025-07-03 10:14:18 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return "Welcome to the Flask App!"
+
+@app.route('/api/data', methods=['GET'])
+
+def get_data():
+    data = {
+        "message": "This is some sample data",
+        "status": "success"
+    }
+    return jsonify(data)
+
+@app.route('/api/data', methods=['POST'])
+
+def post_data():
+    data = request.json
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    return jsonify({"received": data, "status": "success"}), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
