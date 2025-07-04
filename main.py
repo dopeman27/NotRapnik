8fdeaf32475a70a9df3cf42400b2e71f3b655c97
MlodyJacky <source056@gmail.com>
2025-07-04 18:24:38 +0200

    notes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-# main.py
-
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/notes')
+def notes():
+    return render_template('notes.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
