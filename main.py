5fa7e6fa066ce2ec34d59b402a63eef1345e13de
MlodyJacky <source056@gmail.com>
2025-07-04 18:30:41 +0200

    simple scaffold of our app

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def home():
 def notes():
     return render_template('notes.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
